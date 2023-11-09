33b3769d10128b252342a7d5f8d21e0b224b7eb1
Gabriel Souza <gablgdario03@gmail.com>
2023-11-09 17:39:21 -0300

    fix with `CRA`

---
+++ src/Components/About.js
@@ -0,0 +1,30 @@
+import Style from "./About.module.css";
+
+function About() {
+  return (
+    <div>
+      <section className={Style.about} id="about">
+        <div className={Style.main}>
+          <img src="/Img/Foto01.jpg" />
+          <div className={Style.about_text}>
+            <h2>About Me</h2>
+            <h5>
+              Developer <span>& Designer</span>
+            </h5>
+            <p>
+              I am a front-end web developer. I can provide clena code and pixel
+              perfect design. I also make the website more & more interactive
+              with web animations. I can provide clean code and pixel perfect
+              design. I also make the website more & more interactive with web
+              animations. A responsive design makes your website accessible to
+              all users, regardless of their device.
+            </p>
+            <button type="button">Let's Talk</button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default About;
