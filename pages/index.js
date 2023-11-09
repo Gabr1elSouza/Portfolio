2190f3276c3136c60a7ea399664ea1343336be84
Gabriel Souza <gablgdario03@gmail.com>
2023-11-08 22:49:17 -0300

    mend
    fix `image about`

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ function index() {
       {/* About section start */}
       <section className={style.about} id="about">
         <div className={style.main}>
-          <img src="/img/Foto01.jpg" alt="foto-principal" />
+          <img src="/Img/Foto01.jpg" alt="foto-principal" />
           <div className={style.about_text}>
             <h2>About Me</h2>
             <h5>
